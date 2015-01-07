49785c1312d6d431e4aba86491150376830f93ac
jiangyx <jiangyx@ctrip.com>
2015-01-07 12:34:42 +0800

    1、使用变量来保存版本/路径/分支等制作rpm包所需的信息 2、增加命令行参数，-v version用来指定版本，-b branch用来指定远端的分支

--- ctrip_ops_mysql_rpm.sh
+++ ctrip_ops_mysql_rpm.sh
@@ -1,17 +1,115 @@
 #!/bin/bash
 #possible pre-request 
 #sudo yum install rpm-build redhat-rpm-config gcc gcc-c++ cmake make zlib-devel openssl-devel gperf
+
+#variables of version / branch.
+
+#default version is 5.6.21. relative variables' default value list below
+MYSQL_VERSION_DEFAULT="5.6.21"
+REPOSITORY_DEFAULT="http://git.dev.sh.ctripcorp.com/ops-mysql/mysql-5-6-21-ctrip.git"
+SOURCE_DIR_DEFAULT="mysql-5.6.21"
+BRANCH_DEFAULT="master"
+SOURCE_TAR_DEFAULT="mysql-5.6.21.tar.gz"
+SPEC_DEFAULT="mysql.5.6.21.spec"
+
+MYSQL_VERSION=$MYSQL_VERSION_DEFAULT
+
+while getopts "v:b:" arg #选项后面的冒号表示该选项需要参数
+do
+  case $arg in
+  v)
+#    echo "v's arg:$OPTARG" #参数存在$OPTARG中
+    MYSQL_VERSION="$OPTARG"
+    ;;
+  b)
+#    echo "b"
+    BRANCH="$OPTARG"
+echo "$BRANCH"
+    ;;
+  ?)  #当有不认识的选项的时候arg为?
+    echo "unkonw argument"
+    exit 1
+    ;;
+  esac
+done
+
+
+#variables required by execution
+RPM_DIR="ctrip_ops_mysql"
+REPOSITORY=""
+SOURCE_DIR=""
+SOURCE_TAR=""
+#BRANCH=""
+
+#get execution infomation
+#version related variables
+case "$MYSQL_VERSION" in
+5[.-]6[.-]12)
+MYSQL_VERSION="5.6.12"
+REPOSITORY="http://git.dev.sh.ctripcorp.com/ops-mysql/mysql-5-6-12-ctrip.git"
+SOURCE_DIR="mysql-5.6.12"
+SOURCE_TAR="mysql-5.6.12.tar.gz"
+SPEC="mysql.5.6.12.spec"
+;;
+5[.-]6[.-]21)
+MYSQL_VERSION=$MYSQL_VERSION_DEFAULT
+REPOSITORY=$REPOSITORY_DEFAULT
+SOURCE_DIR=$SOURCE_DIR_DEFAULT
+SOURCE_TAR=$SOURCE_TAR_DEFAULT
+SPEC=$SPEC_DEFAULT
+;;
+*)
+echo "unsupported version"
+exit 1
+;;
+esac
+
+#get branch
+if [ -z "$BRANCH" ]
+then
+BRANCH=$BRANCH_DEFAULT
+#echo "BRANCH -z"
+fi
+#echo "$BRANCH"
+
+echo "Building Mysql rpm at version "$MYSQL_VERSION
+echo "Code repository is "$REPOSITORY
+echo "Features are from branch "$BRANCH
+echo "Code directory is "$SOURCE_DIR
+
+#debug for show current infomation 
+#read
+
 cd ~
-mkdir ctrip_ops_mysql
-cd ctrip_ops_mysql
-git clone http://git.dev.sh.ctripcorp.com/ops-mysql/mysql-5-6-21-ctrip.git mysql-5.6.21
+
+if [ -d "$RPM_DIR" ];
+then
+echo "directory "$RPM_DIR" exist.Now remove it!!!!!"
+rm -rf "$RPM_DIR"
+fi
+mkdir $RPM_DIR
+
+#test for branch checkout BRANCH=rpl_port
+cd $RPM_DIR
+git clone $REPOSITORY $SOURCE_DIR
+if (( $? )); then 
+exit 1 ; 
+fi
+cd $SOURCE_DIR
+git checkout -b $BRANCH origin/$BRANCH
+if (( $? )); then 
+exit 1 ; 
+fi
+#echo "git checkout -b $BRANCH origin/$BRANCH"
+cd ..
+
 #rpm directory
 mkdir -p rpm/{BUILD,RPMS,SOURCES,SPECS,SRPMS} tmp
-tar -zcvf mysql-5.6.21.tar.gz mysql-5.6.21
+tar -zcvf $SOURCE_TAR $SOURCE_DIR
 mkdir bld
 cd bld
-cmake ../mysql-5.6.21 -DBUILD_CONFIG=mysql_release
+cmake ../$SOURCE_DIR -DBUILD_CONFIG=mysql_release
 cd ..
 cp bld/support-files/*.spec rpm/SPECS
-cp mysql-5.6.21.tar.gz rpm/SOURCES
-rpmbuild -v --define="_topdir $PWD/rpm" --define="_tmppath $PWD/tmp" -ba rpm/SPECS/mysql.5.6.21.spec
+cp $SOURCE_TAR rpm/SOURCES
+rpmbuild -v --define="_topdir $PWD/rpm" --define="_tmppath $PWD/tmp" -ba rpm/SPECS/$SPEC
